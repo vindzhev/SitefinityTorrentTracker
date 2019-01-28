efdc35a48e30d97dc593514cf726fe56943a4617
v.indzhev <v.indzhev@gmail.com>
2019-01-28 11:22:57 +0200

    Added administration page stylings

--- ResourcePackages/STracker/assets/dist/js/custom.js
+++ ResourcePackages/STracker/assets/dist/js/custom.js
@@ -1,1 +1,8 @@
-﻿
+﻿function updateUploadName(element, defaultText) {
+    var uploadButton = element.parentElement.getElementsByTagName('button')[0];
+    if (uploadButton) {
+        if (element.files.length > 0) {
+            uploadButton.innerHTML = element.value.split('\\').pop();
+        } else { uploadButton.innerHTML = defaultText; }
+    }
+}
